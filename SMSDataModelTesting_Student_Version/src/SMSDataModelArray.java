dbf8afd3f265bb9d75a1cff5493396592d37b92d
electrixocket <electrixocektaus@gmail.com>
2018-05-17 13:03:14 +0930

    finished

--- SMSDataModelTesting_Student_Version/src/SMSDataModelArray.java
+++ SMSDataModelTesting_Student_Version/src/SMSDataModelArray.java
@@ -5,135 +5,137 @@
  * set when the SMSDateModel object is created
  */
 import java.io.Serializable;
+import java.sql.Array;
 import java.util.Arrays;
 
 public class SMSDataModelArray implements SMSDataModelInterface, Serializable {
 
-  private static final long serialVersionUID = 1L;
-  private static final int DEFAULT_MAX_NUM_PHONE_NUMBERS = 20;
-  public static final String FULL = "FULL";
-  public static final String DUPLICATE = "DUPLICATE";
+    private static final long serialVersionUID = 1L;
+    private static final int DEFAULT_MAX_NUM_PHONE_NUMBERS = 20;
+    public static final String FULL = "FULL";
+    public static final String DUPLICATE = "DUPLICATE";
 
-  private String message;         //The SMS message
-  private String[] phoneNumbers;  //The collection of phone numbers
-  private int numPhoneNumbers;    //How many phone numbers there are
+    private String message;         //The SMS message
+    private String[] phoneNumbers;  //The collection of phone numbers
+    private int numPhoneNumbers;    //How many phone numbers there are
 
-  public SMSDataModelArray(String message) {
-    this(message, DEFAULT_MAX_NUM_PHONE_NUMBERS);
-  }
-
-  public SMSDataModelArray() {
-    this("", DEFAULT_MAX_NUM_PHONE_NUMBERS);
-  }
-
-  public SMSDataModelArray(String initialMessage, int maxNumPhoneNumbers) {
-    this.message = initialMessage;
-    this.phoneNumbers = new String[maxNumPhoneNumbers];
-    this.numPhoneNumbers = 0;
-  }
-
-  @Override
-  public String addPhoneNumber(String newPhoneNumber) throws
-    SMSDataModelFullException {
-    String result;
-
-    if (numPhoneNumbers == phoneNumbers.length) {
-      result = FULL;
-    } else {
-      boolean exists;
-      exists = findPhoneNumberIndex(newPhoneNumber) != -1;
-      if (exists) {
-        result = DUPLICATE;
-      } else {
-        phoneNumbers[numPhoneNumbers] = newPhoneNumber;
-        numPhoneNumbers++;
-        result = newPhoneNumber;
-      }
+    public SMSDataModelArray(String message) {
+        this(message, DEFAULT_MAX_NUM_PHONE_NUMBERS);
     }
-    return result;
-  }
 
-  public int findPhoneNumberIndex(String targetNumber) {
-    int i = 0;
-    for (i = 0; i < this.numPhoneNumbers; i++) {
-      if (this.phoneNumbers[i].equals(targetNumber)) {
-        return i;
-      }
+    public SMSDataModelArray() {
+        this("", DEFAULT_MAX_NUM_PHONE_NUMBERS);
     }
-    return -1;
-  }
 
-  public String updatePhoneNumber(String newPhoneNumber, int i) {
-    String result;
-
-    if (i < 0 || i >= numPhoneNumbers) {
-      result = null;
-    } else {
-      result = phoneNumbers[i];
-      phoneNumbers[i] = newPhoneNumber;
+    public SMSDataModelArray(String initialMessage, int maxNumPhoneNumbers) {
+        this.message = initialMessage;
+        this.phoneNumbers = new String[maxNumPhoneNumbers];
+        this.numPhoneNumbers = 0;
     }
-    return result;
-  }
 
-  public String getPhoneNumber(int i) {
-    String result;
+    @Override
+    public String addPhoneNumber(String newPhoneNumber) throws
+            SMSDataModelFullException {
+        String result;
 
-    if (i < 0 || i >= numPhoneNumbers) {
-      result = null;
-    } else {
-      result = phoneNumbers[i];
+        if (numPhoneNumbers == phoneNumbers.length) {
+            result = FULL;
+        } else {
+            boolean exists;
+            exists = findPhoneNumberIndex(newPhoneNumber) != -1;
+            if (exists) {
+                result = DUPLICATE;
+            } else {
+                phoneNumbers[numPhoneNumbers] = newPhoneNumber;
+                numPhoneNumbers++;
+                result = newPhoneNumber;
+            }
+        }
+        return result;
     }
-    return result;
-  }
 
-  public String deleteNumber(int i) {
-    String result;
-    if (i < 0 || i >= numPhoneNumbers) {
-      result = null;
-    } else {
-      result = phoneNumbers[i];
-      if (i != this.numPhoneNumbers - 1) {
-        System.arraycopy(phoneNumbers, i + 1, phoneNumbers, i, this.numPhoneNumbers - i - 1);
-      }
-      this.numPhoneNumbers--;
+    public int findPhoneNumberIndex(String targetNumber) {
+        int i = 0;
+        for (i = 0; i < this.numPhoneNumbers; i++) {
+            if (this.phoneNumbers[i].equals(targetNumber)) {
+                return i;
+            }
+        }
+        return -1;
     }
-    return result;
-  }
 
-  public int getMaxNumPhoneNumbers() {
-    return this.phoneNumbers.length;
-  }
+    public String updatePhoneNumber(String newPhoneNumber, int i) {
+        String result;
 
-  public boolean isFull() {
-    return this.numPhoneNumbers == this.phoneNumbers.length;
-  }
+        if (i < 0 || i >= numPhoneNumbers) {
+            result = null;
+        } else {
+            result = phoneNumbers[i];
+            phoneNumbers[i] = newPhoneNumber;
+        }
+        return result;
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getPhoneNumber(int i) {
+        String result;
 
-  public void setMessage(String mMessage) {
-    this.message = mMessage;
-  }
+        if (i < 0 || i >= numPhoneNumbers) {
+            result = null;
+        } else {
+            result = phoneNumbers[i];
+        }
+        return result;
+    }
 
-  public String[] getPhoneNumbers() {
-    return phoneNumbers;
-  }
+    public String deleteNumber(int i) {
+        String result;
+        if (i < 0 || i >= numPhoneNumbers) {
+            result = null;
+        } else {
+            result = phoneNumbers[i];
+            if (i != this.numPhoneNumbers - 1) {
+                System.arraycopy(phoneNumbers, i + 1, phoneNumbers, i, this.numPhoneNumbers - i - 1);
+            }
+            this.numPhoneNumbers--;
+        }
+        return result;
+    }
 
-  public int getNumPhoneNumbers() {
-    return numPhoneNumbers;
-  }
+    public int getMaxNumPhoneNumbers() {
+        return this.phoneNumbers.length;
+    }
 
-  @Override
-  public String toString() {
-    return "SMSDataModel [message=" + message + ", phoneNumbers="
-      + Arrays.toString(phoneNumbers) + ", numPhoneNumbers="
-      + numPhoneNumbers + "]";
-  }
+    public boolean isFull() {
+        return this.numPhoneNumbers == this.phoneNumbers.length;
+    }
 
-  @Override
-  public void sortNumbers() {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String mMessage) {
+        this.message = mMessage;
+    }
+
+    public String[] getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public int getNumPhoneNumbers() {
+        return numPhoneNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return "SMSDataModel [message=" + message + ", phoneNumbers="
+                + Arrays.toString(phoneNumbers) + ", numPhoneNumbers="
+                + numPhoneNumbers + "]";
+    }
+
+    @Override
+    public void sortNumbers() {
+        Arrays.sort(phoneNumbers);
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
--- SMSDataModelTesting_Student_Version/test/SMSDataModelTesting.java
+++ SMSDataModelTesting_Student_Version/test/SMSDataModelTesting.java
@@ -1,4 +1,8 @@
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,12 +34,14 @@ public class SMSDataModelTesting {
             System.out.println("Testing [adding PhoneNumber] ->" + smsDataModelArray1);
 
             //MexphoneNumber setting
+            System.out.println("*---------------------ARRAY TESTING------------------------*");
             SMSDataModelArray smsDataModelArray2;
             smsDataModelArray2 = new SMSDataModelArray("Setting mex phone number to 2", 2);
 
             System.out.println("Testing [Max PhoneNumber] ->" + smsDataModelArray2);
 
             //Find phone number Index
+            System.out.println("*---------------------------------------------*");
             smsDataModelArray2.addPhoneNumber("123456");
             smsDataModelArray2.addPhoneNumber("654321");
 
@@ -44,6 +50,7 @@ public class SMSDataModelTesting {
             System.out.println("Testing [find PhoneNumber 123456's Index] ->" + smsDataModelArray2.findPhoneNumberIndex("123456"));
 
             //Update phone number
+            System.out.println("*---------------------------------------------*");
             SMSDataModelArray smsDataModelArray3;
             smsDataModelArray3 = new SMSDataModelArray("MaxPN 1", 1);
             smsDataModelArray3.addPhoneNumber("before update");
@@ -53,11 +60,13 @@ public class SMSDataModelTesting {
             System.out.println("Testing [updatePhoneNumber after] ->" + smsDataModelArray3);
 
             //Get phone number
+            System.out.println("*---------------------------------------------*");
             System.out.println("Testing [getPhoneNumber at array 0] ->" + smsDataModelArray2.getPhoneNumber(0));
             System.out.println("Testing [getPhoneNumber at array 1] ->" + smsDataModelArray2.getPhoneNumber(1));
             System.out.println("Testing [getPhoneNumber at array 2] ->" + smsDataModelArray2.getPhoneNumber(2));
 
             //delete number
+            System.out.println("*---------------------------------------------*");
             String phoneNumberdeleted;
             System.out.println("Testing [deleteNumber brfore deletion] ->" + smsDataModelArray2);
             phoneNumberdeleted = smsDataModelArray2.deleteNumber(0);
@@ -70,12 +79,129 @@ public class SMSDataModelTesting {
             System.out.println("Testing [deleteNumber at array 2 expecting Null] ->" + phoneNumberdeleted);
             System.out.println("Testing [deleteNumber at array 2] ->" + smsDataModelArray2);
 
-            //
-            
+            //getMaxNumPhonenumbers
+            System.out.println("*---------------------------------------------*");
+            SMSDataModelArray smsDataModelArray4;
+            smsDataModelArray4 = new SMSDataModelArray("maxis7", 7);
+            System.out.println("Testing [getphoneMaxNumber, expecting 7] ->" + smsDataModelArray4.getMaxNumPhoneNumbers());
+
+            //is full
+            System.out.println("*---------------------------------------------*");
+            smsDataModelArray4.addPhoneNumber("11224456");
+            smsDataModelArray4.addPhoneNumber("16224456");
+            smsDataModelArray4.addPhoneNumber("14224456");
+            smsDataModelArray4.addPhoneNumber("91224456");
+            smsDataModelArray4.addPhoneNumber("81224456");
+            smsDataModelArray4.addPhoneNumber("71224456");
+            smsDataModelArray4.addPhoneNumber("51224456");
+            String answer = "";
+            if (smsDataModelArray4.isFull() == true) {
+                answer = "Full";
+            } else {
+                answer = "No Full";
+            }
+            System.out.println("Testing [isfull, expecting Full] ->" + answer);
+
+            //getPhoneNumbers
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getphoneNumbers] ->" + Arrays.toString(smsDataModelArray4.getPhoneNumbers()));
+
+            //get num phonNumbers
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getphoneMaxNumber, expecting 7] ->" + smsDataModelArray4.getNumPhoneNumbers());
+
+            //getMessage
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getMessage, expecting maxis7] ->" + smsDataModelArray4.getMessage());
+
+            //setMessage
+            System.out.println("*---------------------------------------------*");
+            smsDataModelArray4.setMessage("SetMessage Done");
+            System.out.println("Testing [setMessage, expecting maxis7] ->" + smsDataModelArray4.getMessage());
+
+            //testing sorting
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [Before sorting, expecting maxis7] ->" + smsDataModelArray4);
+            smsDataModelArray4.sortNumbers();
+            System.out.println("Testing [After sorting, expecting maxis7] ->" + smsDataModelArray4);
         } catch (SMSDataModelFullException ex) {
             Logger.getLogger(SMSDataModelTesting.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        try {
+            System.out.println("\n" + "*--------------------LIST TESTING-------------------------*");
+            SMSDataModelList smsDataModelList1;
+            smsDataModelList1 = new SMSDataModelList("message", 5);
+            //Add Phone Number
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [Add phone Number before]->" + smsDataModelList1);
+            smsDataModelList1.addPhoneNumber("1333");
+            smsDataModelList1.addPhoneNumber("12663");
+            smsDataModelList1.addPhoneNumber("76663");
+            smsDataModelList1.addPhoneNumber("82663");
+            smsDataModelList1.addPhoneNumber("62123");
+            System.out.println("Testing [Add phone Number before]->" + smsDataModelList1);
+
+            //findPhoneNumberIndex
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [findPhoneNumberIndex, expecting 2]->" + smsDataModelList1.findPhoneNumberIndex("76663"));
+
+            //updatePhoneNumber
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [updatePhoneNumber before update]->" + smsDataModelList1);
+            smsDataModelList1.updatePhoneNumber("1333", 1);
+            System.out.println("Testing [updatePhoneNumber after update]->" + smsDataModelList1);
+
+            //getPhoneNumber
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getPhoneNumber, expecting 76663]->" + smsDataModelList1.getPhoneNumber(2));
+
+            //deleteNumber
+            System.out.println("Testing [updatePhoneNumber before Delete index0]->" + smsDataModelList1);
+            smsDataModelList1.deleteNumber(0);
+            System.out.println("Testing [updatePhoneNumber after Delete index0]->" + smsDataModelList1);
+
+            //getMaxNumPhoneNumbers
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getMaxPhoneNumbers, expecting 5]->" + smsDataModelList1.getMaxNumPhoneNumbers());
+
+            //isFull
+            System.out.println("*---------------------------------------------*");
+            String answer = "";
+            if (smsDataModelList1.isFull() == true) {
+                answer = "Full";
+            } else {
+                answer = "Not Full";
+            }
+            System.out.println("Testing [isfull, expecting Not Full] ->" + answer);
+
+            //getPhoneNumbers
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getPhoneNumbers]->" + Arrays.toString(smsDataModelList1.getPhoneNumbers()));
+
+            //getNumPhoneNumbers
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getNumPhoneNumbers, expecting 4]->" + smsDataModelList1.getNumPhoneNumbers());
+
+            //get Message
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [getMessage]->" + smsDataModelList1.getMessage());
+
+            //set Message
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [set message before]->" + smsDataModelList1.getMessage());
+            smsDataModelList1.setMessage("this message has Edited");
+            System.out.println("Testing [set message after]->" + smsDataModelList1.getMessage());
+
+            //sort number
+            System.out.println("*---------------------------------------------*");
+            System.out.println("Testing [sort numbers before]->" + smsDataModelList1);
+            smsDataModelList1.sortNumbers();
+            System.out.println("Testing [sort numbers after]->" + smsDataModelList1);
+
+        } catch (SMSDataModelFullException ex) {
+            Logger.getLogger(SMSDataModelTesting.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
